755ebae2b010d29a3c993408c9a650ab4da33245
jordanagk <23035239+jordanagk@users.noreply.github.com>
2020-09-17 11:45:02 -0400

    New Github Site for Fall 2020
    Deleted my old files and started new with the project-one folder.

--- old-site/experiments/animate-scroll/assets/rocket-launch.jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#rocket-button').click(function() {
-    $("#rocket").toggleClass("launch");
-  });
-
-  $('#rocket-button2').click(function() {
-    $("#rocket-2").toggleClass("launch-2");
-  });
-});
--- old-site/experiments/flash-card-flip/assets/card-flip.js
+++
@@ -1,9 +0,0 @@
-var card = document.querySelector('.card');
-card.addEventListener('click', function() {
-  card.classList.toggle('is-flipped');
-});
-
-var card = document.querySelector('#b2');
-card.addEventListener('click', function() {
-  card.classList.toggle('is-flipped');
-});
